f3e548c20c9108bc014da38ada83887626c82117
Ferlucena <ferlucena@hotmail.com>
2021-02-03 17:39:43 -0300

    Codifico metodos y atributos

--- ProyectoSistemaVentas-JScript.js
+++ ProyectoSistemaVentas-JScript.js
@@ -6,4 +6,24 @@
  *
  */
 
+ class Producto{
+     static contadorProductos = 0;
+
+     constructor(nombre, precio){
+         this._idProducto = ++Producto.contadorProductos;
+         this._nombre = nombre;
+         this._precio = precio;        
+     }
+
+     //Getters
+     get idProducto(){
+         return this._idProducto;
+     }
+     get nombre(){
+         return this._nombre;
+     }
+     get precio(){
+         return this._precio
+     }
+ }
  
